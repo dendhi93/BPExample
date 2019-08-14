e305087a040b11b79ec82e7777ad4e51c060b6ff
dracoo <dendhi.fendi@gmail.com>
2019-08-14 09:10:21 +0700

    preparing mainactivity

--- app/src/main/java/com/coba/printapps/feature/MainActivity.java
+++ app/src/main/java/com/coba/printapps/feature/MainActivity.java
@@ -2,6 +2,7 @@ package com.coba.printapps.feature;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +10,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.coba.printapps.R;
+import com.coba.printapps.utils.BluetoothHandler;
+import com.zj.btsdk.BluetoothService;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks, BluetoothHandler.HandlerInterface {
     TextView et_text, tv_status;
     Button btn_print_image;
     ImageView imageView;
+
+    private final String TAG = MainActivity.class.getSimpleName();
+    public static final int RC_BLUETOOTH = 0;
+    public static final int RC_CONNECT_DEVICE = 1;
+    public static final int RC_ENABLE_BLUETOOTH = 2;
+
+    private BluetoothService mService = null;
+    private boolean isPrinterReady = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStart(){
+        initBluetooth();
         btn_print_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,5 +53,39 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @AfterPermissionGranted(RC_BLUETOOTH)
+    private void initBluetooth() {
+        String[] params = {Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN};
+        if (!EasyPermissions.hasPermissions(this, params)) {
+            EasyPermissions.requestPermissions(this, "You need bluetooth permission",
+                    RC_BLUETOOTH, params);
+            return;
+        }
+        mService = new BluetoothService(this, new BluetoothHandler(this));
+    }
 
+
+    @Override
+    public void onDeviceConnected() {
+        isPrinterReady = true;
+        tv_status.setText("Terhubung dengan perangkat");
+    }
+
+    @Override
+    public void onDeviceConnecting() { tv_status.setText("Sedang menghubungkan..."); }
+
+    @Override
+    public void onDeviceConnectionLost() {
+        isPrinterReady = false;
+        tv_status.setText("Koneksi perangkat terputus");
+    }
+
+    @Override
+    public void onDeviceUnableToConnect() {tv_status.setText("Tidak dapat terbuhubung ke perangkat"); }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) { }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) { }
 }
