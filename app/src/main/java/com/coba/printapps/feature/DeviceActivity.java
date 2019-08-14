e8f7c946650d441248203ef78b727218c4370a37
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-08-15 05:54:37 +0700

    add device activity

--- app/src/main/java/com/coba/printapps/feature/DeviceActivity.java
+++ app/src/main/java/com/coba/printapps/feature/DeviceActivity.java
@@ -7,8 +7,10 @@ import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -18,9 +20,11 @@ import android.widget.Toast;
 import com.coba.printapps.R;
 import com.zj.btsdk.BluetoothService;
 
+import java.util.Set;
+
 public class DeviceActivity extends AppCompatActivity {
     TextView title_paired_devices,title_new_devices;
-    ListView paired_devices,new_devices;
+    ListView lvPaired_devices,lvNew_devices;
     Button button_scan;
 
     public static final String EXTRA_DEVICE_ADDRESS = "device_address";
@@ -33,20 +37,48 @@ public class DeviceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_device);
         title_paired_devices = findViewById(R.id.title_paired_devices);
         title_new_devices = findViewById(R.id.title_new_devices);
-        paired_devices = findViewById(R.id.paired_devices);
-        new_devices = findViewById(R.id.new_devices);
+        lvPaired_devices = findViewById(R.id.paired_devices);
+        lvNew_devices = findViewById(R.id.new_devices);
         button_scan = findViewById(R.id.button_scan);
     }
 
     @Override
     protected void onStart(){
         super.onStart();
+        mService = new BluetoothService(this, null);
+        Set<BluetoothDevice> pairedDevice = mService.getPairedDev();
+
         button_scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(DeviceActivity.this, "Under Maintenance", Toast.LENGTH_SHORT).show();
+                doDiscovery();
+                button_scan.setVisibility(View.GONE);
             }
         });
+
+        ArrayAdapter<String> pairedDeviceAdapter = new ArrayAdapter<>(this, R.layout.list_item);
+        lvPaired_devices.setAdapter(pairedDeviceAdapter);
+        lvPaired_devices.setOnItemClickListener(mDeviceClickListener);
+
+        ArrayAdapter<String> newDeviceAdapter = new ArrayAdapter<>(this, R.layout.list_item);
+        lvNew_devices.setAdapter(newDeviceAdapter);
+        lvNew_devices.setOnItemClickListener(mDeviceClickListener);
+
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, intentFilter);
+        intentFilter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(mReceiver, intentFilter);
+
+        if (pairedDevice.size() > 0) {
+            title_paired_devices.setVisibility(View.VISIBLE);
+            for (BluetoothDevice device : pairedDevice) {
+                pairedDeviceAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        } else {
+            String noDevice = "Tidak ada perangkat terhubung!";
+            pairedDeviceAdapter.add(noDevice);
+            button_scan.setVisibility(View.VISIBLE);
+        }
     }
 
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -66,5 +98,42 @@ public class DeviceActivity extends AppCompatActivity {
             }
         }
     };
+
+    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            mService.cancelDiscovery();
+
+            String info = ((TextView) view).getText().toString();
+            String address = info.substring(info.length() - 17);
+
+            Intent intent = new Intent();
+            intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    };
+
+    private void doDiscovery() {
+        setTitle("Mencari perangkat...");
+        title_new_devices.setVisibility(View.VISIBLE);
+
+        if (mService.isDiscovering()) {
+            mService.cancelDiscovery();
+        }
+
+        mService.startDiscovery();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mService != null) {
+            mService.cancelDiscovery();
+        }
+        mService = null;
+        unregisterReceiver(mReceiver);
+    }
 
 }
